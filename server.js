6c49ae5a681a2a89d6c853ee3f81919d0adb1c20
martinnord <martin.nordstrom99@gmail.com>
2019-07-19 21:35:08 +0200

    test

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const corsOptions = {
-  origin: "https://www.coworkingbuddies.com",
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*"
+  // allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 const validEmail = yup
@@ -54,10 +54,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // });
 
 app.post("/email_received", cors(corsOptions), async (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://www.coworkingbuddies.com"
-  );
+  // res.setHeader(
+  //   "Access-Control-Allow-Origin",
+  //   "https://www.coworkingbuddies.com"
+  // );
   if (req.body.email === "") return res.send("Please provide an email");
 
   try {

04f5916e51e819c11d490bd41a4ab61f7d85428d
martinnord <martin.nordstrom99@gmail.com>
2019-07-19 21:41:19 +0200

    test

--- server.js
+++ server.js
@@ -38,6 +38,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // app.get("/email_list", async (req, res) => {
 //   try {
 //     const emails = await Email.find();
@@ -53,7 +77,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   // });
 // });
 
-app.post("/email_received", cors(corsOptions), async (req, res) => {
+app.post("/email_received", async (req, res) => {
   // res.setHeader(
   //   "Access-Control-Allow-Origin",
   //   "https://www.coworkingbuddies.com"
